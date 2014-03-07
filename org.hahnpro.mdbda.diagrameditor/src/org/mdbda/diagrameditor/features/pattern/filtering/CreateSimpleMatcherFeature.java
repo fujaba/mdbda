d9630cfcdb500fc2a9982ecb341b3c2848e0429d
Marcel Hahn <mh@hahnpro.com>
2014-03-07 15:07:02 +0100

    some changes

--- org.hahnpro.mdbda.diagrameditor/src/org/mdbda/diagrameditor/features/pattern/filtering/CreateSimpleMatcherFeature.java
+++ org.hahnpro.mdbda.diagrameditor/src/org/mdbda/diagrameditor/features/pattern/filtering/CreateSimpleMatcherFeature.java
@@ -13,27 +13,16 @@ public class CreateSimpleMatcherFeature extends CreatePatternFeature implements
 	public static String name = "SimpleMatcher";
 	public static String description = "Creates a new simple matcher filter pattern";
     
-    
 	public CreateSimpleMatcherFeature(IFeatureProvider fp) {
 		super(fp, name, description);
 	}
-
-
-
+	
 	@Override
 	public Object[] create(ICreateContext context) {
-		// TODO: create the domain object here
-		//Object newDomainObject = null;
-		
-
 		Pattern eInst = ModelFactory.eINSTANCE.createPattern();
 		eInst.setTypeId(FilteringPatternTemplateConstatns.SimpleMatcherFilter);
 		
 		addToWorkflow(getWorkflow(context), eInst);
-		
-		// TODO: in case of an EMF object add the new object to a suitable resource
-//		getDiagram().eResource().getContents().add(eInst);
-		
 		
 		addGraphicalRepresentation(context, eInst);
 		return new Object[] { eInst };
